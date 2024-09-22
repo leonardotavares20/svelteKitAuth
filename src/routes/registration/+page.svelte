86ffe79265301fa5fc255a1574672f02cdf0e9cf
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-22 01:57:09 -0300

    feat: add registration route

---
+++ src/routes/registration/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import type { ActionData } from "./$types";
+
+  export let data: PageData;
+  export let form: ActionData;
+</script>
+
+<div class="w-svw h-svh">
+  <form action="" method="post">
+    <div>
+      <label for="username" id="username">Username</label>
+      <input type="text" name="username" placeholder="Username" />
+    </div>
+    <div>
+      <label for="password" id="password">Password</label>
+      <input type="password" name="password" placeholder="Password" />
+    </div>
+  </form>
+</div>
