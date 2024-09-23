ee88cd3a95d634fc8f4f0e7c2e38e17411a09ddc
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-22 22:52:39 -0300

    refactor: use form store of svkit and add validation messages

--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -1,20 +1,52 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
-  import type { ActionData } from "./$types";
+  import { page } from '$app/stores';
+  import type { ActionData } from './$types';
 
-  export let data: PageData;
-  export let form: ActionData;
+  const form = $page.form as ActionData;
 </script>
 
-<div class="w-svw h-svh">
-  <form action="" method="post">
-    <div>
-      <label for="username" id="username">Username</label>
-      <input type="text" name="username" placeholder="Username" />
+<div class="w-svw h-svh flex items-center justify-center">
+  <form class="w-svw h-svh grid grid-cols-2" action="?/register" method="post">
+    <div class="grid grid-rows-2">
+      <div class="h-full w-full flex flex-col justify-end items-center pb-4">
+        <input
+          class="text-white text-2xl w-3/4 h-20 px-3 transition-all bg-[#101014] border-0 border-b-2 border-orange-400 outline-none placeholder:text-white focus:border-orange-500"
+          type="text"
+          value={form?.register?.formData?.username ?? ''}
+          name="username"
+          autocomplete="off"
+          placeholder="Username"
+        />
+        {#if form?.username}
+          <p class="text-red-500 text-lg">
+            {form.username.message}
+          </p>
+        {/if}
+        {#if form?.register?.fieldErrors?.username}
+          <p class="text-red-500 text-lg">
+            {form?.register.fieldErrors?.username[0]}
+          </p>
+        {/if}
+      </div>
+      <div class="h-full w-full flex flex-col justify-start items-center pt-4">
+        <input
+          class="text-white text-2xl w-3/4 h-20 px-3 transition-all bg-[#101014] border-0 border-b-2 border-orange-400 outline-none placeholder:text-white focus:border-orange-500"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        {#if form?.register?.fieldErrors?.password}
+          <p class="text-red-500 text-lg">
+            {form?.register.fieldErrors?.password[0]}
+          </p>
+        {/if}
+      </div>
     </div>
-    <div>
-      <label for="password" id="password">Password</label>
-      <input type="password" name="password" placeholder="Password" />
+    <div class="h-full">
+      <button
+        class="h-full w-full transition-all text-7xl hover:bg-orange-500"
+        type="submit">Register</button
+      >
     </div>
   </form>
 </div>
