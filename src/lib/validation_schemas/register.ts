647c300a22722a06c153771ab52ae8b41821c6c8
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-22 22:49:14 -0300

    refactor: remove passwordConfirm of register validation schema

--- src/lib/validation_schemas/register.ts
+++ src/lib/validation_schemas/register.ts
@@ -1,19 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const registerSchema = z.object({
   username: z
-    .string({ required_error: "Name is required" })
-    .min(3, { message: "Name must be more than 3 characters" })
-    .max(64, { message: "Name must be less than 64 characters" })
+    .string({ required_error: 'Name is required' })
+    .min(3, { message: 'Name must be more than 3 characters' })
+    .max(64, { message: 'Name must be less than 64 characters' })
     .trim(),
   password: z
     .string()
-    .min(6, { message: "Password must be more than 6 characters" })
-    .max(32, { message: "Password must be less than 32 characters" })
-    .trim(),
-  confirmPassword: z
-    .string()
-    .min(6, { message: "Password must be more than 6 characters" })
-    .max(32, { message: "Password must be less than 32 characters" })
+    .min(6, { message: 'Password must be more than 6 characters' })
+    .max(32, { message: 'Password must be less than 32 characters' })
     .trim(),
 });
