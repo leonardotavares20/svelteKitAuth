824f2511a5d6981786c9ea4c79c95d5f2b9b91f2
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-24 01:23:54 -0300

    refactor(registration): change input style

--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -10,7 +10,7 @@
     <div class="grid grid-rows-2">
       <div class="h-full w-full flex flex-col justify-end items-center pb-4">
         <input
-          class="text-white text-2xl w-3/4 h-20 px-3 transition-all bg-[#101014] border-0 border-b-2 border-orange-400 outline-none placeholder:text-white focus:border-orange-500"
+          class="text-white text-2xl w-3/4 h-20 px-3 transition-all bg-[#101014] border-0 border-b-2 border-orange-400 outline-none placeholder:text-white focus:border-white"
           type="text"
           value={form?.register?.formData?.username ?? ''}
           name="username"
@@ -30,7 +30,7 @@
       </div>
       <div class="h-full w-full flex flex-col justify-start items-center pt-4">
         <input
-          class="text-white text-2xl w-3/4 h-20 px-3 transition-all bg-[#101014] border-0 border-b-2 border-orange-400 outline-none placeholder:text-white focus:border-orange-500"
+          class="text-white text-2xl w-3/4 h-20 px-3 transition-all bg-[#101014] border-0 border-b-2 border-orange-400 outline-none placeholder:text-white focus:border-white"
           type="password"
           name="password"
           placeholder="Password"
